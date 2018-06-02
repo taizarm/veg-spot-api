c43c2cca29956335ddf2a1cb74ac480f38364411
Taiza <taizarabello@gmail.com>
2018-06-02 17:28:57 -0300

    Change of directory name from "vegspotapi" to "config".

--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'vegspotapi.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'vegspotapi.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
